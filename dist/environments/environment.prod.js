3e5a7200eed0976e9e748c5e1f0b1cfd31717a8f
Plopez11 <Pedrolopeze11@gmail.com>
2022-10-17 19:24:19 -0400

    fixed error ELF header

--- dist/environments/environment.prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    production: true,
-    baseUrl: "const mongodb://localhost:27017/condominios",
-};

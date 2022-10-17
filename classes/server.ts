099e380b4346e96a12f513a21cc26616808effa2
Plopez11 <Pedrolopeze11@gmail.com>
2022-10-16 23:16:59 -0400

    make it better

--- classes/server.ts
+++ classes/server.ts
@@ -1,15 +1,18 @@
 import express from 'express'
 import { environment } from '../environments/environment';
-
+// import {dotenv} from '../'
 
  
 
 export default class Server {
 
+    public port = process.env.PORT || 3000;
+    public host = process.env.HOST || '0.0.0.0';
+
     public app : express.Application;
     // public port: number = 3000;
-    public host=environment.HOST || '0.0.0.0';
-    public port=environment.PORT || 3000;
+    // public host=environment.HOST || '0.0.0.0';
+    // public port=environment.PORT || 3000;
     
 
     constructor() {

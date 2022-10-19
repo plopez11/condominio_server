0a04a26c92af2ac66a2e6fd563a5d2c38bd9d940
Plopez11 <Pedrolopeze11@gmail.com>
2022-10-18 21:02:41 -0400

    make it better

---
+++ dist/environments/environment.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = void 0;
+exports.environment = {
+    PROD: true,
+    baseUrl: "const mongodb://localhost:27017/condominios",
+    MONGODB_URI: "mongodb+srv://mern_user:TBGFt2d8k6J9uCa@miclustercafe.v8qne.mongodb.net/condominios?retryWrites=true&w=majority",
+    HOST: "Localhost",
+    PORT: ""
+};
